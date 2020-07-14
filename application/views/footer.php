c0d63ee904b9f57301cbb3073dc5fcf197194181
Bruno Sampaio <brunomurer@hotmail.com>
2020-07-14 01:39:51 -0300

    Adicionado Jquery mask
    Adicionados campos de endereço no formulario.

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,12 +2,12 @@
 <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 <script src="<?php echo base_url('assets/js/jquery-1.12.4.min.js') ?>"></script> 
 <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.mask.min.js') ?>"></script>
 <script type="text/javascript">
     $(document).ready(function () {
-        /* MASK */
-        $('.cnpj').mask('00.000.000/0000-00', {placeholder: "cnpj"});
-        $('.cpf').mask('000.000.000-00', {placeholder: "CPF"});
-        $('.nascimento').mask('00/00/0000', {placeholder: "Nascimento"});
+        /* MASK */       
+        $('.numeroCpf').mask('000.000.000-00', {placeholder: "número do CPF"});
+        $('.dataNascimento').mask('00/00/0000', {placeholder: "Data de nascimento"});
 		
 	});
 </script>
--- application/views/home.php
+++ application/views/home.php
@@ -3,38 +3,67 @@
     <div class="panel panel-default">
       <div class="panel-body">
         <h2 style="margin-top:0px">Cadastrar paciente </h2>
-         <?php if ($this->session->flashdata('message')) : ?>
+        <?php if ($this->session->flashdata('message')) : ?>
         <div class="alert alert-info" role="alert" align="center"> <?php echo $this->session->flashdata('message') ?> </div>
         <?php endif; ?>
         <?php if ($this->session->flashdata('error')) : ?>
         <div class="alert alert-danger" role="alert" align="center"> <?php echo $this->session->flashdata('error') ?> </div>
         <?php endif; ?>
         <form class="" action="" method="post">
-        <div class="form-group">
-            <label for="varchar">Nome completo <?php echo form_error('nomeCompleto') ?></label>
-            <input type="text" class="form-control" name="nomeCompleto" id="nomeCompleto" placeholder="Nome completo do titular" value="<?php echo $nomeCompleto; ?>" />
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="varchar">Nome completo <?php echo form_error('nomeCompleto') ?></label>
+                <input type="text" class="form-control" name="nomeCompleto" id="nomeCompleto" placeholder="Nome completo do titular" value="<?php echo $nomeCompleto; ?>" />
+              </div>
+              <div class="form-group">
+                <label for="varchar">Nome da mãe <?php echo form_error('nomeMae') ?></label>
+                <input type="text" class="form-control" name="nomeMae" id="nomeMae" placeholder="Nome da Mãe" value="<?php echo $nomeMae; ?>" />
+              </div>
+              <div class="form-group">
+                <label for="varchar">Data de nascimento <?php echo form_error('dataNascimento') ?></label>
+                <input type="date" class="form-control" name="dataNascimento" id="dataNascimento" placeholder="00/00/0000"  value="<?php echo $dataNascimento; ?>" />
+              </div>
+              <div class="form-group">
+                <label for="varchar">CPF <?php echo form_error('numeroCpf') ?></label>
+                <input type="text" class="form-control" name="numeroCpf" id="numeroCpf" placeholder="Numero do Cpf" value="<?php echo $numeroCpf; ?>" />
+              </div>
+              <div class="form-group">
+                <label for="varchar">Numero do cartao de saúde<?php echo form_error('numeroCartao') ?></label>
+                <input type="text" class="form-control" name="numeroCartao" id="numeroCartao" placeholder="Numero do cartao de saúde" value="<?php echo $numeroCartao; ?>" />
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="varchar">Endereço<?php echo form_error('endereco') ?></label>
+                <input type="text" class="form-control" name="endereco" id="endereco" placeholder="Nome da rua/Avenida" value="<?php echo $endereco; ?>" />
+              </div>
+              <div class="form-group">
+                <label for="varchar">Número<?php echo form_error('numero') ?></label>
+                <input type="text" class="form-control" name="numero" id="numero" placeholder="Número" value="<?php echo $numero; ?>" />
+              </div>
+              <div class="form-group">
+                <label for="varchar">Cep<?php echo form_error('cep') ?></label>
+                <input type="text" class="form-control" name="cep" id="cep" placeholder="Número" value="<?php echo $cep; ?>" />
+              </div>
+              <div class="form-group">
+                <label for="varchar">Bairro<?php echo form_error('bairro') ?></label>
+                <input type="text" class="form-control" name="bairro" id="bairro" placeholder="Bairro" value="<?php echo $bairro; ?>" />
+              </div>
+              <div class="form-group">
+                <label for="varchar">Cidade<?php echo form_error('cidade') ?></label>
+                <input type="text" class="form-control" name="cidade" id="cidade" placeholder="Cidade" value="<?php echo $cidade; ?>" />
+              </div>
+              <div class="form-group">
+                <label for="varchar">Estado<?php echo form_error('estado') ?></label>
+                <input type="text" class="form-control" name="estado" id="estado" placeholder="Estado" value="<?php echo $estado; ?>" />
+              </div>
+            </div>
           </div>
           <div class="form-group">
-            <label for="varchar">Nome da mãe <?php echo form_error('nomeMae') ?></label>
-            <input type="text" class="form-control" name="nomeMae" id="nomeMae" placeholder="Nome da Mãe" value="<?php echo $nomeMae; ?>" />
-          </div>
-          <div class="form-group">
-            <label for="varchar">Data de nascimento <?php echo form_error('dataNascimento') ?></label>
-            <input type="date" class="form-control" name="dataNascimento" id="dataNascimento" placeholder="00/00/0000"  value="<?php echo $dataNascimento; ?>" />
-          </div>
-          <div class="form-group">
-            <label for="varchar">CPF <?php echo form_error('numeroCpf') ?></label>
-            <input type="text" class="form-control" name="numeroCpf" id="numeroCpf" placeholder="Numero do Cpf" value="<?php echo $numeroCpf; ?>" />
-          </div>
-          <div class="form-group">
-            <label for="varchar">Numero do cartao de saúde<?php echo form_error('numeroCartao') ?></label>
-            <input type="text" class="form-control" name="numeroCartao" id="numeroCartao" placeholder="Numero do cartao de saúde" value="<?php echo $numeroCartao; ?>" />
-          </div>
-          <div class="form-group">
-            <input type="hidden" name="id" value="<?php echo $id; ?>" /> 
+            <input type="hidden" name="id" value="<?php echo $id; ?>" />
             <button type="submit" class="btn btn-primary">Salvar</button>
-            <a href="<?php echo site_url('patients') ?>" class="btn btn-default">Cancelar</a>
-          </div>
+            <a href="<?php echo site_url('patients') ?>" class="btn btn-default">Cancelar</a> </div>
         </form>
       </div>
     </div>
