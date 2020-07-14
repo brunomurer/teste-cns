b19d1ebd414eff686c7c5677a261d3c3ea270c43
Bruno Sampaio <brunomurer@hotmail.com>
2020-07-13 21:54:17 -0300

    Corrigida exibição do formulario.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,44 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+ public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');		
+		$this->load->model('form_validation');
+
+	}
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		
+				if ($this->input->post()){
+					
+					
+				}
+		
+		    $data = array(
+            'button' => 'Salvar',
+            'action' => site_url('patients/create'),
+	    'id' => set_value('id'),
+	    'nomeCompleto' => set_value('nomeCompleto'),
+	    'nomeMae' => set_value('nomeMae'),
+	    'dataNascimento' => set_value('dataNascimento'),
+	    'numeroCpf' => set_value('numeroCpf'),
+	    'numeroCartao' => set_value('numeroCartao'),
+	    'cep' => set_value('cep'),
+		'endereco' => set_value('endereco'),
+		'numero' => set_value('numero'),
+		'cidade' => set_value('cidade'),
+		'bairro' => set_value('bairro'),
+		'estado' => set_value('estado'),
+	);
+	
+	    $this->load->view('header');	
+		$this->load->view('home', $data);
+		$this->load->view('footer');
 	}
 }
